57f241dff72ad99f6fc2bd51ad4d92e7965ab1de
NiordFresh <squiavaquila@gmail.com>
2025-09-17 21:32:02 +0200

    removed polish comments
    removed polish comments

--- MiniWindowsUpdateManager/Properties/AssemblyInfo.cs
+++ MiniWindowsUpdateManager/Properties/AssemblyInfo.cs
@@ -3,9 +3,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
-// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
-// powiązane z zestawem.
 [assembly: AssemblyTitle("MiniWUManager (Canary)")]
 [assembly: AssemblyDescription("MiniWUManager is a lightweight, portable tool for Windows systems that allows you to have full control over Windows Update.")]
 [assembly: AssemblyConfiguration("")]
@@ -15,21 +12,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("https://github.com/NiordFresh/MiniWUManager")]
 [assembly: AssemblyCulture("")]
 
-// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
-// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
-// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
 [assembly: ComVisible(false)]
 
-// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
 [assembly: Guid("06ca6f34-0273-4e54-b060-d4bca60f59d8")]
 
-// Informacje o wersji zestawu zawierają następujące cztery wartości:
-//
-//      Wersja główna
-//      Wersja pomocnicza
-//      Numer kompilacji
-//      Poprawka
-//
 [assembly: AssemblyVersion("0.25.09.17")]
 [assembly: AssemblyFileVersion("0.25.09.17")]
 [assembly: NeutralResourcesLanguage("en")]
